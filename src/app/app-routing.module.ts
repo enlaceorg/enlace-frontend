ad59b244f9840a797ceff1ed9b0f079d10752e5c
jhonatanferreira94 <jhonjhonpf@hotmail.com>
2022-06-02 11:28:03 -0300

    Atualização

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
 
+  {path:"", redirectTo:"inicio", pathMatch:"full"},
   {path:"login", component: LoginComponent},
+  {path:"cadastrar", component: CadastrarComponent},
+  {path:"inicio", component: InicioComponent},
 
 ];
 
